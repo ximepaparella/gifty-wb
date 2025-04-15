03879dbbc61f56576ccd6721fa27fb594ddef395
ximepaparella <ximenapaparella@gmail.com>
2025-04-14 23:14:28 -0300

    Add headers to all the services for security

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -4,6 +4,7 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
+    'X-API-Key': import.meta.env.VITE_API_KEY,
   },
 });
 
